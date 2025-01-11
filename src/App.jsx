2cf382f41cdcc7c1062b4a2e4265364ae9d3816a
lucasolv505 <155476756+tridentam@users.noreply.github.com>
2025-01-11 14:16:09 -0300

    minor correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-  import { Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import './App.css'
+import './Index.css'
 
 function App() {
 
